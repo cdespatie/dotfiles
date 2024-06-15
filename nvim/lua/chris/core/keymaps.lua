83fe8033e59e26a28fba9953f37aabe35344a9e0
Chris Despatie <mail@chrisdespatie.com>
2024-06-15 12:43:50 -0400

    Initial vim keymaps

---
+++ nvim/lua/chris/core/keymaps.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("n", "<leader><leader>", ".", { desc = "Repeat last action by tapping leader twice" })
+
+keymap.set("n", "<leader>s", "<c-W>v", { desc = "Create vertical split" })
+keymap.set("n", "<leader>h", "<c-W>s", { desc = "Create horizontal split" })
+keymap.set("n", "<leader>w", "<c-W>w", { desc = "Move clockwise through splits" })
+keymap.set("n", "<leader>q", "<c-W>W", { desc = "Move counter-clockwise through splits" })
+
+-- Fat finger workarounds for :W and :Q
+vim.api.nvim_create_user_command('W', function() pcall(vim.cmd("w")) end, {})
+vim.api.nvim_create_user_command('Q', function() pcall(vim.cmd("q")) end, {})
---
+++ nvim/lua/chris/core/options.lua
@@ -0,0 +1,25 @@
+local opt = vim.opt
+
+opt.number = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.breakindent = true
+
+opt.swapfile = false
+
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
