32c370dbbc3f76c21a5059b840ea127880782ef1
Chris Despatie <cdespatie@wealthsimple.com>
2024-06-19 11:06:58 -0400

    Terraform support and gitignore

--- nvim/lua/chris/plugins/init.lua
+++ nvim/lua/chris/plugins/init.lua
@@ -1,3 +1,4 @@
 return {
   "nvim-lua/plenary.nvim", -- Package of lua functions used by many other plugins
+  "hashivim/vim-terraform", -- Syntax highlighting and basic functionality for terraform
 }
