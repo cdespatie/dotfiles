9ed352643ab026e75eeb7aefca4f4fe67d28ce40
Chris Despatie <cdespatie@wealthsimple.com>
2024-07-31 10:25:28 -0400

    zsh config

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ mkdir -p ~/.config
 # Set up symlinks
 ln -s ~/gitrepos/dotfiles/nvim ~/.config
 ln -s ~/gitrepos/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/gitrepos/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/gitrepos/dotfiles/zsh/starship.toml ~/.config/starship.toml
