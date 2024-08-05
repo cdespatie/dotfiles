294e2f0790b98af6dc2307b709061bafe2e5d6bc
Chris Despatie <mail@chrisdespatie.com>
2024-08-05 11:04:13 -0400

    Updating install script

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ mkdir -p ~/.config
 # Set up symlinks
 ln -s ~/gitrepos/dotfiles/nvim ~/.config
 ln -s ~/gitrepos/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -s ~/gitrepos/dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/gitrepos/dotfiles/zsh/starship.toml ~/.config/starship.toml
+
+# Copy over symlink for now due to secrets
+cp ~/gitrepos/dotfiles/zsh/.zshrc ~/.zshrc
